d441d854a48576e29d6120ae0d66b80ffa11d7bc
LynnAkinyi <lynnakinyi2003@gmail.com>
2025-05-01 18:17:39 +0300

    allowed hosts

--- tradingbot/settings.py
+++ tradingbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zvgvx19x&yqivyqdd#9c#$)6zdte3i0#*e)eh^u1x7i%npi(h7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
